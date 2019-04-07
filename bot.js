089e23ae07bc29a2738a74ec626caa321efc33ad
ShadowLilly <44298093+shadowlilly@users.noreply.github.com>
2019-04-07 14:38:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ bot.on('ready',async function (evt) {
 	//7562D3E5-0ADA-4CAE-9867-61FF528A657B.jpeg
 	setInterval(function() {
 		var currentTime = time();
-		if(!medsSentToday && currentTime[0] > 11) {
+		if(!medsSentToday && currentTime[0] > 4) {
 			bot.fetchUser("353849640640315392").then(function(user) {
 				user.send({
 					files: [{attachment: "images/7562D3E5-0ADA-4CAE-9867-61FF528A657B.jpeg", name: "meds.jpg"}]
@@ -27,7 +27,7 @@ bot.on('ready',async function (evt) {
 			});
 			medsSentToday = true;
 		}
-		else if(time[0] < 22) {
+		else if(time[0] < 4) {
 			medsSentToday = false;
 		}
 	}, 15000);
